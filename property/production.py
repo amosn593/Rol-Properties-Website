21905bd3bea5d26b2a7dd518e06216630d3a14f2
Amos Ndonga <amosndonga@gmail.com>
2021-09-28 12:40:55 +0300

    changed psycopg2 version

--- property/production.py
+++ property/production.py
@@ -39,11 +39,11 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
